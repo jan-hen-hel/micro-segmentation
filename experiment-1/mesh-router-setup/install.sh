d2dcacd65fcbe90b400d148fc1a23f2bf0baa974
Jan Lühr <jan@simula.no>
2024-11-01 11:35:31 +0100

    Wireless config

--- experiment-1/mesh-router-setup/install.sh
+++ experiment-1/mesh-router-setup/install.sh
@@ -1,20 +1,20 @@
 #!/bin/sh
 opkg update
 
-# 0. Workaround for missing hostname command
+echo "0. Workaround for missing hostname command"
 ## https://github.com/openwrt/openwrt/issues/11765
 echo "cat /proc/sys/kernel/hostname" > /sbin/hostname
 chmod 755 /sbin/hostname
 
-# 1. OVS - package and ports
+echo "1. OVS - package and ports"
 opkg install openvswitch
 
-# Workaround for buggy OpenVSwtich startup scripts
+echo "Workaround for buggy OpenVSwtich startup scripts"
 cp rc.local /etc/
 chmod 755 /etc/rc.local
 /etc/rc.local
 
-## Create iot bridge
+echo "Create iot bridge"
 ovs-vsctl add-br iot
 
 ## Ports for VLAN-interfaces created by hostapd
@@ -29,9 +29,15 @@ echo "Creating a lan-port at the OVS-bridge that can be utilized to distribute l
 ovs-vsctl add-port iot iotupl -- set Interface iotupl type=internal
 
 ## Add Port eth2 (wired IoT-Port to the bridge
+echo "Adding eth2 (wired IoT-Port) to OVS bridge"
 ovs-vsctl add-port iot eth2
 
 ## Create OpenWRT network configuration for the IoT bridge
-uci import -m < network_iot_if.uci
+echo "Defining UCI network configuration for OVS bridge-ports"
+uci -m import network < network_iot_if.uci
+uci add_List network.@device[0].ports='iotupl'
 uci commit network
 
+echo "Setting up WLAN-AP for iot network"
+uci -m import wireless < iot_wireless.uci
+uci commit wireless
