509f823e47696ac97c86695f775cd4f63a57c90c
jan-hen-hel <163833962+jan-hen-hel@users.noreply.github.com>
2024-11-05 17:28:17 +0100

    Changes for node 12

--- experiment-1/flow_rules_apu_12.sh
+++ experiment-1/flow_rules_apu_12.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
 # Rules for APU-12 (Mesh-Node)
-ovs-ofctl --protocols=OpenFlow13 del-flows iot
+ovs-ofctl del-flows iot
+
+# Populate table 1 and 2 for MPLS handling of incoming traffic
+sh flow_rules_mpls_untag.sh 
 
 # APU has two giot devices: APU-22
 # Both are isolated. There is nothing local. Hence, after ingress filtering (to prevent MAC-spoofing) all traffic is forwarded to the MAC-interface
@@ -11,23 +14,19 @@ ovs-ofctl --protocols=OpenFlow13 del-flows iot
 # No optimizations.
 
 ## APU-GIOT-22
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iot_22, dl_src=00:0A:52:06:E4:31, dl_dst=01:00:00:00:00:00/01:00:00:00:00:00, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8848,set_mpls_label=22,iotupl"
+ovs-ofctl add-flow iot "table=0, in_port=iot_22, dl_src=00:0A:52:06:E4:31, dl_dst=01:00:00:00:00:00/01:00:00:00:00:00, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8848,set_mpls_label=22,iotupl"
 
 
 ## Same rule for unicast-traffic. Prevent MAC-Spoofing by ingres Filter and ecapsulate
 ## APU-GIOT-22
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iot_22, dl_src=00:0A:52:06:E4:31, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8847,set_mpls_label=22,iotupl"
+ovs-ofctl add-flow iot "table=0, in_port=iot_22, dl_src=00:0A:52:06:E4:31, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8847,set_mpls_label=22,iotupl"
 
-# Incoming
+# Incoming - iot upl
+ovs-ofctl  add-flow iot "table=0, in_port=iotupl, actions=resubmit(,1)"
 
-# Broadcast-Traffic from Gateway
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iotupl, eth_type=0x8848,mpls_label=10, actions=pop_mpls:0x8848,move:mpls_label[0..15]->metadata[0..15],pop_mpls:metadata[0..15],iot_22"
-# Broadcast-Traffic from APU-22 to APU-20
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iotupl, eth_type=0x8847,mpls_label=21, actions=pop_mpls:0x8847,iot_22"
+# Traffic from Gateway
+ovs-ofctl  add-flow iot "table=10,metadata[1..20]=10,actions=iot_22"
 
-# Unicast-Traffic from Gateway to APU-22
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iotupl,dl_dst=00:0A:52:06:E4:31, eth_type=0x8847,mpls_label=0x10, actions=pop_mpls:0x8847, iot_22"
-
-# Unicast-Traffic from APU-22 to APU-20
-ovs-ofctl --protocols=OpenFlow13 add-flow iot "table=0, in_port=iotupl,dl_dst=00:0A:52:06:E4:31, eth_type=0x8847,mpls_label=0x20, actions=pop_mpls:0x8847, iot_22"
+# Traffic from APU-22 to APU-20
+ovs-ofctl  add-flow iot "table=10,metadata[1..20]=20,actions=iot_22"
 
