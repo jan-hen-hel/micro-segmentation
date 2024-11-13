ef945234962decfcb98968fe7b77bc9e0a85fed8
Jan Lühr <jan@simula.no>
2024-11-13 17:15:36 +0100

    fixed flow-rules

--- experiment-2/flow_rules_apu_11.sh
+++ experiment-2/flow_rules_apu_11.sh
@@ -14,27 +14,19 @@ sh flow_rules_mpls_untag.sh
 
 ## Brodcast
 ## No optimizations.
-ovs-ofctl add-flow iot "table=0, in_port=iot_20, dl_src=00:0A:52:06:E4:32, dl_dst=01:00:00:00:00:00/01:00:00:00:00:00, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8848,set_mpls_label=20,iotupl"
-ovs-ofctl add-flow iot "table=0, in_port=iot_21, dl_src=00:0A:52:06:E4:2A, dl_dst=01:00:00:00:00:00/01:00:00:00:00:00, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8848,set_mpls_label=21,iotupl"
+
+ovs-ofctl add-flow iot "table=0, in_port=iot_31, dl_src=f8:ad:cb:03:8e:ea, dl_dst=01:00:00:00:00:00/01:00:00:00:00:00, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8848,set_mpls_label=31,iotupl"
 
 
 ## Unicast
-## Same rule for unicast-traffic. Prevent MAC-Spoofing by ingres Filter and ecapsulate
-ovs-ofctl add-flow iot "table=0, in_port=iot_20, dl_src=00:0A:52:06:E4:32, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8847,set_mpls_label=20,iotupl"
-ovs-ofctl add-flow iot "table=0, in_port=iot_21, dl_src=00:0A:52:06:E4:2A, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8847,set_mpls_label=21,iotupl"
+## Same rule for unicast-traffic. Prevent MAC-Spoofing by ingres Filter and encapsulate
+ovs-ofctl add-flow iot "table=0, in_port=iot_31, dl_src=f8:ad:cb:03:8e:ea, actions=move:eth_type->metadata[0..15],push_mpls:0x8848,move:metadata[0..15]->mpls_label[0..15],push_mpls:0x8847,set_mpls_label=31,iotupl"
 
 
 # Incoming 
 ## iot upl - remove MPLS
 ovs-ofctl  add-flow iot "table=0, in_port=iotupl, actions=resubmit(,1)"
 
-## Broadcast-Traffic from Gateway
-ovs-ofctl  add-flow iot "table=10,metadata[1..20]=10,metadata[0]=1,actions=iot_20,iot_21"
-
-## Unicast-Traffic from Gateway
-ovs-ofctl  add-flow iot "table=10,metadata[1..20]=10,metadata[0]=0,dl_dst=00:0A:52:06:E4:32,actions=iot_20"
-ovs-ofctl  add-flow iot "table=10,metadata[1..20]=10,metadata[0]=0,dl_dst=00:0A:52:06:E4:2A,actions=iot_21"
-
-## Traffic from APU-22 to APU-20
-ovs-ofctl  add-flow iot "table=10,metadata[1..20]=22,actions=iot_20"
-
+# Smartphone is exposed - forward any broadcast traffic as well as unicast-traffic
+ovs-ofctl  add-flow iot "table=10,metadata[0]=1,actions=iot_31"
+ovs-ofctl  add-flow iot "table=10,metadata[0]=0,dl_dst=f8:ad:cb:03:8e:ea,actions=iot_31"
